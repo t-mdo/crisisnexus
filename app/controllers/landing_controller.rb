f3f987810da1a20e6dc8f5d514b47320ceac6d37
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-10-06 01:25:50 +0200

    Add other routes for lp

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,10 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def pricing
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  
+  get '/pricing', to: 'landing#pricing'
+  get '/contact', to: 'landing#contact'
   root "landing#index"
 end
