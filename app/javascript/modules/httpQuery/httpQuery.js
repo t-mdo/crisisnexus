6907a0d0471160cfc2181af272b47af9db34b727
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-10-18 00:42:24 +0200

    Add back httpQuery

---
+++ app/javascript/modules/httpQuery/httpQuery.js
@@ -0,0 +1,36 @@
+const environment =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+const URLS = {
+  development: 'http://127.0.0.1:3000',
+  production: 'https://www.crisisnexus.com',
+};
+
+const getUrl = ({ url, params }) => {
+  const urlObj = new URL(url, URLS[environment]);
+
+  Object.entries(params).forEach(([key, value]) => {
+    urlObj.searchParams.append(key, value);
+  });
+  return urlObj.href;
+};
+
+const httpQuery = async ({ url, method = 'GET', body = {}, params = {} }) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  const response = await fetch(getUrl({ url, params }), {
+    method,
+    headers,
+    body: method !== 'GET' && JSON.stringify(body),
+  });
+
+  const data = await response.json().catch(() => {});
+
+  if (!response.ok) throw { ...response, message: data?.message };
+  return { ...response, data: await data };
+};
+
+export default httpQuery;
