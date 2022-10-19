d261112ee1ddaf19c24e3b55e38ab13962381706
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-10-19 17:10:01 +0200

    Add basic home

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -1,7 +1,25 @@
+import useHttpQuery from 'modules/httpQuery/useHttpQuery';
+import Alert from 'components/Alert';
+import Loader from 'components/Loader';
+
+const OpenCrisisDashboard = () => {};
+
+const ClosedCrisisDashboard = () => <p></p>;
+
 const Home = () => {
+  const { loading, success, error, data, status } = useHttpQuery({
+    url: '/incidents/open',
+  });
+
   return (
-    <div>
-      <h1>No on-going incident at the moment</h1>
+    <div className="py-6 px-4">
+      <h2 className="text-3xl">Dashboard</h2>
+      {loading && <Loader />}
+      {success && <OpenCrisisDashboard />}
+      {error && status === 404 && <ClosedCrisisDashboard />}
+      {error && status !== 404 && (
+        <Alert type="error">An error occured while loading the dashboard</Alert>
+      )}
     </div>
   );
 };
