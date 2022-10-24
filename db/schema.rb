de89d2ddd27e8aabf0cbb8b7740ee331cbd868d5
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-10-24 23:59:08 +0200

    Add phone number to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_215616) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_215843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_215616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "organization_id", null: false
+    t.string "phone_number"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["organization_id"], name: "index_accounts_on_organization_id"
   end
