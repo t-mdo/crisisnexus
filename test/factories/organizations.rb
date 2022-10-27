2847e64b1bc30d2e21f593a41a7181d9ca4d3210
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-10-27 16:55:35 +0200

    Fix organization factory with identifier

--- test/factories/organizations.rb
+++ test/factories/organizations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :organization do
-    name { Faker::Company.unique.name }
+    name { Faker::Company.name }
+    identifier { Faker::Internet.unique.domain_name }
   end
 end
