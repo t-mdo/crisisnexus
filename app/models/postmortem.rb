8fc63e3f7790dd9b0a0d37f7b1f46deb11a405c6
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-11-10 23:49:21 +0100

    Add pm validation on start/end consistency

--- app/models/postmortem.rb
+++ app/models/postmortem.rb
@@ -4,6 +4,12 @@ class Postmortem < ApplicationRecord
   has_many :next_step_actions
   accepts_nested_attributes_for :next_step_actions, allow_destroy: true
 
+  validates :incident_impact_ended_at,
+            comparison: {
+              greater_than: :incident_impact_started_at
+            },
+            allow_nil: true
+
   def touched?
     updated_at > created_at
   end
--- test/models/postmortem_test.rb
+++ test/models/postmortem_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class PostmortemTest < ActiveSupport::TestCase
+  test 'impact start/end timestamps should be consistent' do
+    account = create(:account)
+    incident = create(:incident, creator: account)
+    postmortem = Postmortem.new(
+      incident:,
+      assigned_to: account,
+      incident_impact_started_at: Time.now,
+      incident_impact_ended_at: Time.now - 1.hour
+    )
+    assert_not postmortem.valid?
+    assert_match(/Incident impact ended at must be greater than/, postmortem.errors.full_messages.join)
+  end
+
   test 'touched?' do
-    postmortem = create(:postmortem)
+    postmortem = create(:postmortem, incident: create(:incident))
     assert_not postmortem.touched?
     postmortem.update!(summary: 'summary')
     assert postmortem.touched?
