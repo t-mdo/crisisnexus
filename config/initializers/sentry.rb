070d3e9018ed92064d0594ebc938d866931a7660
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-11-13 15:07:34 +0100

    Only enable sentry in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.dsn = 'https://2d827c9adaf24147a63e9a2aa1aeb14a@o191897.ingest.sentry.io/4504141786120192'
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
   config.traces_sample_rate = 1.0
+  config.enabled_environments = %w[production]
 end
