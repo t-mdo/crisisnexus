6fdec85073e8b3db0b213ccf0bd64da47c40c158
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-11-13 14:36:57 +0100

    Add sentry for the front

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/react';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 import App from 'pages/App';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://e0dc0fe2372849158c87afce8131219c@o191897.ingest.sentry.io/4504152066555904',
+  });
+}
 
 dayjs.extend(localizedFormat);
 dayjs.extend(duration);
