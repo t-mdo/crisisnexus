177bc0ebd921a9d11eaced62672587544c54e39e
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-11-18 18:27:58 +0100

    Make email consistent with name

--- test/factories/accounts.rb
+++ test/factories/accounts.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :account do
-    email { Faker::Internet.unique.email }
+    display_name { Faker::Name.unique.name }
+    email { Faker::Internet.email(name: display_name) }
     phone_number { "+336#{Faker::Number.number(digits: 8)}" }
     organization
     activation_state { 'active' }
     onboarding_completed { true }
-    display_name { Faker::Name.unique.name }
 
     trait :enrolled_as_incident_manager do
       after(:create) do |account|
