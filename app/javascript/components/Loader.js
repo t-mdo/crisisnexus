09ad8265339f5685fccc593a09c53b99abbb6423
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-11-27 21:03:29 +0100

    Replace loader string with animated icon

--- app/javascript/components/Loader.js
+++ app/javascript/components/Loader.js
@@ -1,3 +1,10 @@
-const Loader = () => <div>Loading...</div>;
+import classnames from 'classnames';
+import Spinner from 'icons/duotone/spinner-third.svg';
+
+const Loader = ({ className }) => (
+  <Spinner
+    className={classnames('w-6 fill-violet-700 animate-spin', className)}
+  />
+);
 
 export default Loader;
