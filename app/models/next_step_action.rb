98a172b1670e9bfa24c1f23df3145950d73f913a
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-12-11 15:47:18 +0100

    Validate due_at and add account association

--- app/models/next_step_action.rb
+++ app/models/next_step_action.rb
@@ -1,5 +1,10 @@
 class NextStepAction < ApplicationRecord
   belongs_to :postmortem
+  belongs_to :assigned_to, class_name: 'Account', optional: true
 
   validates :name, presence: true
+  validates :due_at,
+            comparison: {
+              greater_than: Time.now.utc
+            }, allow_nil: true
 end
