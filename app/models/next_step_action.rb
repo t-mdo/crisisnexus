fef422c0cbd77146fb2d94949d14850d6437c044
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-12-14 22:16:07 +0100

    Add reverse association

--- app/models/next_step_action.rb
+++ app/models/next_step_action.rb
@@ -1,6 +1,7 @@
 class NextStepAction < ApplicationRecord
   belongs_to :postmortem
   belongs_to :assigned_to, class_name: 'Account', optional: true
+  has_one :incident, through: :postmortem
 
   validates :name, presence: true
   validates :due_at,
