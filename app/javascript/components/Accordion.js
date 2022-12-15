07d31e81a14b14ffffd647b9cb1f11b9021c5a79
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-12-15 19:01:13 +0100

    Add checkbox and complete ui for todos

--- app/javascript/components/Accordion.js
+++ app/javascript/components/Accordion.js
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Transition } from '@headlessui/react';
 import ChevronUpIcon from 'icons/regular/chevron-up.svg';
 
 export const Accordion = Disclosure;
@@ -13,20 +13,31 @@ export const AccordionHeader = ({ className, children, ...props }) => (
     {...props}
   >
     {children}
-    <ChevronUpIcon className="w-3 h-3 fill-gray-900 ui-open:rotate-180 ui-open:transform transition duration-300" />
+    <div className="ml-3">
+      <ChevronUpIcon className="w-3 h-3 fill-gray-900 ui-open:rotate-180 ui-open:transform transition duration-300" />
+    </div>
   </Disclosure.Button>
 );
 
 export const AccordionBody = ({ className, children, ...props }) => (
-  <Disclosure.Panel
-    className={classnames(
-      'p-3 ui-not-open:rounded ui-open:rounded-b bg-stone-200',
-      className,
-    )}
-    {...props}
+  <Transition
+    enter="transition duration-100 ease-out"
+    enterFrom="transform scale-95 opacity-0"
+    enterTo="transform scale-100 opacity-100"
+    leave="transition duration-75 ease-out"
+    leaveFrom="transform scale-100 opacity-100"
+    leaveTo="transform scale-95 opacity-0"
   >
-    {children}
-  </Disclosure.Panel>
+    <Disclosure.Panel
+      className={classnames(
+        'p-3 ui-not-open:rounded ui-open:rounded-b bg-stone-200',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Disclosure.Panel>
+  </Transition>
 );
 
 export default { Accordion, AccordionHeader };
--- app/javascript/pages/home/TodoList.js
+++ app/javascript/pages/home/TodoList.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import classnames from 'classnames';
 import useHttpQuery from 'modules/httpQuery/useHttpQuery';
 import { BlockLoader } from 'components/Loader';
 import Text from 'components/Text';
@@ -8,17 +10,42 @@ import {
   AccordionHeader,
   AccordionBody,
 } from 'components/Accordion';
+import { Checkbox } from 'components/Checkbox';
 import intersperse from 'modules/helpers/intersperse';
 
 const TODO_TYPE_POSTMORTEM = 'postmortem';
 const TODO_TYPE_NEXT_STEP_ACTION = 'next_step_action';
 
-const TodoItem = ({ todo: { type, action_name } }) => {
+const TodoItem = ({ todo: { type, action_name, completed_at } }) => {
+  const [done, setDone] = useState(Boolean(completed_at));
+
   if (type === TODO_TYPE_POSTMORTEM) {
-    return <Text>Publish the postmortem</Text>;
+    return (
+      <div className="flex items-center gap-3 py-1 px-3">
+        <Checkbox disabled />
+        <Text>Publish the postmortem</Text>
+      </div>
+    );
   }
   if (type === TODO_TYPE_NEXT_STEP_ACTION) {
-    return <Text>Action: {action_name}</Text>;
+    return (
+      <div className="flex items-center gap-3 p-2">
+        <Checkbox
+          value={done}
+          onChange={() => {
+            setDone((state) => {
+              return !state;
+            });
+          }}
+        />
+        <Text
+          className={classnames({ 'line-through': done })}
+          color={done ? 'text-gray-500' : null}
+        >
+          Action: {action_name}
+        </Text>
+      </div>
+    );
   }
   return null;
 };
@@ -29,6 +56,7 @@ const TodoList = () => {
     loading: fetchLoading,
     error: fetchError,
     success: fetchSuccess,
+    refresh: fetchRefresh,
   } = useHttpQuery({ url: '/todos' });
 
   if (fetchLoading) return <BlockLoader />;
@@ -44,10 +72,14 @@ const TodoList = () => {
         {intersperse(
           Object.entries(todos).map(([incidentId, incidentTodos]) => (
             <Accordion key={incidentId} defaultOpen>
-              <AccordionHeader>{`Crisis #${incidentId}: ${incidentTodos[0].incident_name}`}</AccordionHeader>
+              <AccordionHeader className="text-left">{`Crisis #${incidentId}: ${incidentTodos[0].incident_name}`}</AccordionHeader>
               <AccordionBody>
                 {incidentTodos.map((todo) => (
-                  <TodoItem key={todo.id} todo={todo} />
+                  <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    fetchRefresh={fetchRefresh}
+                  />
                 ))}
               </AccordionBody>
             </Accordion>
