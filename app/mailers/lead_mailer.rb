17dfe6300d58900bd8d7bd15db356c2939dbfe50
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2022-12-27 20:14:23 +0100

    Remove unnecessary mailer method and view

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -3,9 +3,4 @@ class LeadMailer < ApplicationMailer
     @lead = lead
     mail to: 'tmo@crisisnexus.com', subject: "New Lead registered: #{@lead.email}"
   end
-
-  def lead_creation_email(lead)
-    @lead = lead
-    mail from: 'tmo@crisisnexus.com', to: @lead.email, subject: 'Early access to CrisisNexus'
-  end
 end
